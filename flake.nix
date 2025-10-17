01564621da6ef1e76e84d4baedf2060fddedcde6
musjj <72612857+musjj@users.noreply.github.com>
2025-10-17 23:36:40 +0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "A Bevy CLI tool and linter";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system}.extend rust-overlay.overlays.default;
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          openssl
+        ];
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in
+      {
+        packages.default = rustPlatform.buildRustPackage {
+          pname = "bevy";
+          version = "0.1.0-dev";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          doCheck = false;
+          cargoBuildFlags = [ "--all" ];
+          nativeBuildInputs = nativeBuildInputs ++ (with pkgs; [ makeBinaryWrapper ]);
+          inherit buildInputs;
+          postInstall = ''
+            for bin in $out/bin/bevy{,_lint}; do
+              wrapProgram $bin --set BEVY_LINT_SYSROOT ${toolchain}
+            done
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs;
+        };
+      }
+    );
+}
